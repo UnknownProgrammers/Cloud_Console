44c9b9ec38a680a2ff5de84f02005ad08d6aed64
CyanFrost <patricknopely@gmail.com>
2014-11-15 23:38:22 -0600

    Updated LoginListener
    Renamed the main @EventHandler method of this class to _v1 as to state
    that it is listening to the version 1 of the LoginObject Class. Also
    improved on the login checker's logic statement.

--- src/com/bluefrost/nio/servernclient/listeners/LoginListener.java
+++ src/com/bluefrost/nio/servernclient/listeners/LoginListener.java
@@ -1,41 +1,24 @@
 package com.bluefrost.nio.servernclient.listeners;
 
-import bluefrost.serializable.objects.v1.LoggedInSuccessObject;
-import bluefrost.serializable.objects.v1.LoginObject;
-
 import com.bluefrost.nio.servernclient.events.EventSystemWrapper.EventSystem.EventHandler;
 import com.bluefrost.nio.servernclient.events.EventSystemWrapper.EventSystem.Listener;
 import com.bluefrost.nio.servernclient.main.Main;
 import com.bluefrost.nio.servernclient.useraccess.ClientManager;
-import com.bluefrost.nio.servernclient.useraccess.ClientManager.Client;
 import com.bluefrost.sql.light.usermanagement.UserBase.User;
 
 public class LoginListener implements Listener{
 
 	@EventHandler
-	public void onLoginEvent(LoginObject event){
+	public void _v1(bluefrost.serializable.objects.v1.LoginObject event){
 		try{
 			User u;
 			if((u = Main.getDefaultUserBase().validateUser(event.getUsername(), event.getPassword()))!= null){
-				System.out.println(event.getSocketChannel().getLocalAddress()+ " Logged In As " +event.getUsername() );
-				synchronized(ClientManager.map){
-					Client c = ClientManager.map.inverse().get(event.getSocketChannel());
-					c.username= event.getUsername();
-					c.password = event.getPassword();
-					c.loggedin = true;
-					c.userData = u;
-					Main.getNIOS().send(event.getSocketChannel(), new LoggedInSuccessObject().toByteArray());
-					//Key k = Crypto.randomAESKey();
-					//c.setKey(k);
-					//Main.getNIOS().send(event.getSocketChannel(),new KeyObject(k).encrypt(event.getKey()).toByteArray());
-						//c.setKey();
-				}
-			}else{
-				System.out.println(event.getSocketChannel().getLocalAddress()+ " Failed To Log In As " +event.getUsername() );
+				ClientManager.get(event.getSocketChannel()).set(u);
+				System.out.println("User " + u.username+" Logged In!");
+				Main.getNIOS().send(event.getSocketChannel(), new bluefrost.serializable.objects.v1.LoggedInSuccessObject().toByteArray());
 			}
-		}catch(Exception e){e.printStackTrace();}
+		}catch(Exception e){}
 	}
-	
 	
 	
 	
