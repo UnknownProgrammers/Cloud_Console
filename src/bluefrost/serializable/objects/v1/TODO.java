48469a918fe1f2c7698da1aa48129ba547f68e8e
CyanFrost <patricknopely@gmail.com>
2014-11-18 20:53:41 -0600

    Created TODO
    Things that will need to be done will be listed here.

---
+++ src/bluefrost/serializable/objects/v1/TODO.java
@@ -0,0 +1,10 @@
+package bluefrost.serializable.objects.v1;
+
+public class TODO {
+
+	/*
+	 *  [ ] Find a better Serialization way.
+	 *  [ ] Test new Serialization with AES(128) Encryption
+	 *  
+	 */
+}
